546d124681caa1574da6cc832054540debfad8b1
AlpinooO <alpino.cdc@gmail.com>
2025-04-20 18:02:22 +0200

    Add initial HTML layout and JavaScript functions for car management features

--- src/test/java/com/example/Concess/ConcessApplicationTests.java
+++ src/test/java/com/example/Concess/ConcessApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.Concess;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,9 @@ class ConcessApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Car car = new Car("AA11BB", "Ferrai", 2000);
+		String plateNumber = car.getPlateNumber();
+		assertEquals(plateNumber, "AA11BB");
 	}
 
 }
